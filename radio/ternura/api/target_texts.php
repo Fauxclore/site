f3244c9245e3ea9cc6e71173e2cbf0f394def45f
Fauxclore <thefauxclore@gmail.com>
2025-07-10 15:58:55 +0100

    Adicionei a parte da música e resolvi conflitos (acho eu)

--- radio/ternura/api/target_texts.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-require 'config.php';
-
-// busca até 50 textos aleatórios
-$stmt = $pdo->query('SELECT content FROM ternura ORDER BY RAND() LIMIT 50');
-$texts = $stmt->fetchAll(PDO::FETCH_COLUMN);
-echo json_encode($texts);
--- radio/ternura/api/time.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode(time() * 1000); // Return current Unix timestamp in milliseconds
-?>
