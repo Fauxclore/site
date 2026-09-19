a58f480539cdbb4e4ff6e6e0de9bbd3a266e5e57
Fauxclore <thefauxclore@gmail.com>
2026-09-19 08:37:36 +0100

    Editei o link da webjam para ter mais interesse (com um countdown)

--- .eleventy.js
+++ .eleventy.js
@@ -12,9 +12,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/music");
   eleventyConfig.addPassthroughCopy("src/papers");
   eleventyConfig.addPassthroughCopy({ "src/style.css": "style.css" });
-  eleventyConfig.addPassthroughCopy({ "src/style_n.css": "style_n.css" });
-  eleventyConfig.addPassthroughCopy({ "src/style_h.css": "style_h.css" });
-  eleventyConfig.addPassthroughCopy({ "src/style_m.css": "style_m.css" });
+  eleventyConfig.addPassthroughCopy({
+    "src/style_webjam.css": "style_webjam.css",
+  });
   eleventyConfig.addPlugin(footnotes, {
     title: "Notas",
     titleId: "footnotes-label",
